3f2b6c96cb6b0a754561587b8a748ea9ed6e6f23
omar <omarhrn@gmail.com>
2025-01-21 14:04:49 -0300

    Añadido un proyecto de todotomorrow

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,17 @@ import SectionTitle from './SectionTitle'
 import ProjectCard from './ProjectCard'
 import {v4 as uuidv4} from 'uuid'
 import muscari from '../assets/images/projects/muscari.webp'
+import toDoTomorrow from '../assets/images/projects/todotomorrow.webp'
 
 const projectList = [
+  {
+    title: 'To Do Tomorrow',
+    description: 'To Do Tomorrow es una aplicación web que permite a los usuarios crear y gestionar listas de tareas. Cuenta con un diseño minimalista y una interfaz de usuario intuitiva.',
+    technologies: ['react', 'css', 'redux'],
+    img: toDoTomorrow,
+    repoLink: 'https://github.com/omar15hr/ToDoTomorrow',
+    liveLink: 'https://todotomorrow-omar.netlify.app/'
+  },
   {
     title: 'Muscari Clothing',
     description: 'Muscari es una plataforma de venta de ropa de moda con una interfaz de usuario moderna. Cuenta con pasarela de pago, autenticación de usuarios, y una base de datos de productos y clientes.',
